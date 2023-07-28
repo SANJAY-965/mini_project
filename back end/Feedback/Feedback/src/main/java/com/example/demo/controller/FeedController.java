6ab052a07624b4c14a877eb16b925c31c7b5786c
SANJAY-B <113884661+SANJAY-965@users.noreply.github.com>
2023-07-28 11:50:08 +0530

    Add files via upload

---
+++ back end/Feedback/Feedback/src/main/java/com/example/demo/controller/FeedController.java
@@ -0,0 +1,45 @@
+package com.example.demo.controller;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.FeedBack;
+
+import com.example.demo.service.FeedService;
+
+@RestController
+@CrossOrigin("http://localhost:3000")
+//@RequestMapping("/Feedback")
+public class FeedController {
+	@Autowired
+	private FeedService stuservice;
+
+	@GetMapping("/getfeed")
+	public List<FeedBack> getDetails(){
+		return stuservice.getAllDetails();
+		
+	}
+	@PostMapping("/addfeed")
+	public FeedBack postDetails(@RequestBody FeedBack f) {
+		return stuservice.saveDetails(f);
+	}
+	@DeleteMapping("/deletefeed/{feed_id_id}")
+	public String delete(@PathVariable int feed_id) {
+		 stuservice.deleteDepartmentById(feed_id);
+		 return "Deleted";
+	}
+	@PutMapping("/updatefeed/{feed_id}")
+	public FeedBack update(@PathVariable int feed_id,@RequestBody FeedBack f) {
+	return stuservice.update(feed_id,f);
+	}  
+}
